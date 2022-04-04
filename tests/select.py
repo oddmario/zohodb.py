6ce65de4169c09578f1b17c4e5720b3a25e9ea3d
Mario <32080032+oddmario@users.noreply.github.com>
2022-04-04 19:15:02 +0200

    Delete select.py

--- tests/select.py
+++
@@ -1,8 +0,0 @@
-from zohodb import zohodb
-
-handler = zohodb.ZohoAuthHandler("", "")
-db = zohodb.ZohoDB(handler, [
-    "mydb"
-])
-query = db.select(table="users", criteria='"username" = "mario"')
-print(query)
